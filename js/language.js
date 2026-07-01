317eada6611730ae2c11203bc9693b9b3f178264
mayankagggrawal-eng <mayankagggrawal@gmail.com>
2026-07-01 17:08:37 +0530

    t

--- js/language.js
+++ js/language.js
@@ -5,12 +5,15 @@ window.addEventListener("load", () => {
     const loader = document.getElementById("loader");
     const languageBox = document.querySelector(".language-box");
 
+    languageBox.classList.remove("show");
+
     setTimeout(() => {
 
         loader.classList.add("hide");
+
         languageBox.classList.add("show");
 
-    }, 3000);
+    },3000);
 
 });
 
