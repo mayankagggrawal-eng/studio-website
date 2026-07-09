e954ed465a9c7e3250318d06a3b87779a661f6b6
mayankagggrawal-eng <mayankagggrawal@gmail.com>
2026-07-09 17:05:01 +0530

    y

--- js/eventsData.js
+++ js/eventsData.js
@@ -5,7 +5,7 @@ window.galleryData = [
 
 
   // ==========================
-  // Maternity Shoot
+  // Maternity 
   // ==========================
 {
   category: "Maternity Shoot",
@@ -43,7 +43,55 @@ window.galleryData = [
   category: "Maternity Shoot",
   image: "../../assets/services/events/meternity/01 (9).webp"
 },
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (10).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (11).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (12).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (13).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (14).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (15).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (16).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (16).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (17).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (18).webp"
+},
 
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (19).webp"
+},
+{
+  category: "Maternity Shoot",
+  image: "../../assets/services/events/meternity/01 (20).webp"
+},
 
 
 ]
