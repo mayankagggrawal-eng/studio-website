c8256d52eb3e47d174c066abab7e081c89b51f2b
mayankagggrawal-eng <mayankagggrawal@gmail.com>
2026-07-09 16:56:20 +0530

    Update prewedding.js

--- js/prewedding.js
+++ js/prewedding.js
@@ -23,43 +23,43 @@ window.galleryData = [
 
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (2).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (2).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (3).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (3).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (4).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (4).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (5).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (5).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (6).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (6).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (7).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (7).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (8).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (8).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (9).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (9).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (10).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (10).webp"
 },
 {
   category: "haldi",
-  image: "../../assets/services/pre-wedding/haldi/01 (11).webp"
+  image: "../../assets/services/pre-wedding/haldi/A (11).webp"
 },
 
 
@@ -73,36 +73,36 @@ window.galleryData = [
 
   {
   category: "mehendi",
-  image: "../../assets/services/pre-wedding/mehendi/01 (1).webp"
+  image: "../../assets/services/pre-wedding/mehendi/A (1).webp"
 },
 
 {
   category: "mehendi",
-  image: "../../assets/services/pre-wedding/mehendi/01 (2).webp"
+  image: "../../assets/services/pre-wedding/mehendi/A (2).webp"
 },
 {
   category: "mehendi",
-  image: "../../assets/services/pre-wedding/mehendi/01 (3).webp"
+  image: "../../assets/services/pre-wedding/mehendi/A (3).webp"
 },
 {
   category: "mehendi",
-  image: "../../assets/services/pre-wedding/mehendi/01 (4).webp"
+  image: "../../assets/services/pre-wedding/mehendi/A (4).webp"
 },
 {
   category: "mehendi",
-  image: "../../assets/services/pre-wedding/mehendi/01 (5).webp"
+  image: "../../assets/services/pre-wedding/mehendi/A (5).webp"
 },
 {
   category: "mehendi",
-  image: "../../assets/services/pre-wedding/mehendi/01 (6).webp"
+  image: "../../assets/services/pre-wedding/mehendi/A (6).webp"
 },
 {
   category: "mehendi",
-  image: "../../assets/services/pre-wedding/mehendi/01 (7).webp"
+  image: "../../assets/services/pre-wedding/mehendi/A (7).webp"
 },
 {
   category: "mehendi",
-  image: "../../assets/services/pre-wedding/mehendi/01 (8).webp"
+  image: "../../assets/services/pre-wedding/mehendi/A (8).webp"
 },
 
 
@@ -113,39 +113,39 @@ window.galleryData = [
 
   {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (1).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (1).webp"
 },
 {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (2).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (2).webp"
 },
 {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (3).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (3).webp"
 },
 {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (4).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (4).webp"
 },
 {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (5).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (5).webp"
 },
 {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (6).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (6).webp"
 },
 {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (7).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (7).webp"
 },
 {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (8).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (8).webp"
 },
 {
   category: "Engagement",
-  image: "../../assets/services/pre-wedding/Engagement/01 (9).webp"
+  image: "../../assets/services/pre-wedding/Engagement/A (9).webp"
 },
 
 
@@ -156,62 +156,62 @@ window.galleryData = [
 
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (1).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (1).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (2).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (2).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (3).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (3).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (4).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (4).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (5).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (5).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (6).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (6).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (7).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (7).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (8).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (8).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (9).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (9).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (10).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (10).webp"
 },{
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (11).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (11).webp"
 },{
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (12).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (12).webp"
 },{
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (13).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (13).webp"
 },{
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (14).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (14).webp"
 },{
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (15).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (15).webp"
 },
 {
   category: "Portraits",
-  image: "../../assets/services/pre-wedding/Portraits/01 (16).webp"
+  image: "../../assets/services/pre-wedding/Portraits/A (16).webp"
 },
 
 
